831bc089fea36fd70550abdb62a30ae9afbaca13
ryan <105075689+rpdav@users.noreply.github.com>
2024-06-22 14:41:00 -0400

    Set up home-manager as NixOS module

--- flake.nix
+++ flake.nix
@@ -17,15 +17,16 @@
     nixosConfigurations = {
       nixbook = lib.nixosSystem {
         inherit system;
-        modules = [ ./configuration.nix ];
-      };
-    };
-    homeConfigurations = {
-      ryan = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [ ./home.nix ];
+        modules = [ 
+          ./configuration.nix 
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.ryan = import ./home.nix;
+          };
+        ];
       };
     };
   };
-
 }
