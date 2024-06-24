c2b1aea7e00eb7eaca1de19009831d9b799766e9
ryan <105075689+rpdav@users.noreply.github.com>
2024-06-23 21:54:16 -0400

    added impermanence back after troubleshooting

--- flake.nix
+++ flake.nix
@@ -6,36 +6,38 @@
     nixpkgs.url = "nixpkgs/nixos-24.05";
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-#    impermanence.url = "github:nix-community/impermanence";
-#    impermanence.inputs.nixpkgs.follows = "nixpkgs";
+    impermanence.url = "github:nix-community/impermanence";
+    impermanence.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, home-manager, impermanence, ... }:
     let
       lib = nixpkgs.lib;
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
     nixosConfigurations = {
-      nixbook = lib.nixosSystem {
-        inherit system;
-        modules = [ 
-          ./configuration.nix 
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.ryan = import ./home.nix;
-          }
-        ];
+      nixbook = {
+        lib.nixosSystem {
+          inherit system;
+          modules = [ 
+            ./configuration.nix 
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.ryan = import ./home.nix;
+            }
+          ];
+        };
+        nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            impermanence.nixosModules.impermanence
+            .configuration.nix
+          ];
+        };
       };
-#      nixbook = nixpkgs.lib.nixosSystem {
-#        inherit system;
-#        modules = [
-#          impermanence.nixosModules.impermanence
-#          .configuration.nix
-#        ];
-#      };
     };
   };
 }
