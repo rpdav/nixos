ff3532581460322c584fa47c0066e7b6bba3e552
ryan <105075689+rpdav@users.noreply.github.com>
2024-06-25 12:32:19 -0400

    added impermanence

--- home.nix
+++ home.nix
@@ -54,28 +54,28 @@
   };
 
   # Impermanence
-#  home.persistence."/persist/home/ryan" = {
-#    directories = [
-#      "Downloads"
-#      "Music"
-#      "Pictures"
-#      "Documents"
-#      "Videos"
-#      ".gnupg"
-#      ".ssh"
-#      ".nixops"
-#      ".local/share/keyrings"
-#      ".local/share/direnv"
+  home.persistence."/persist/home/ryan" = {
+    directories = [
+      "Downloads"
+      "Music"
+      "Pictures"
+      "Documents"
+      "Videos"
+      ".gnupg"
+      ".ssh"
+      ".nixops"
+      ".local/share/keyrings"
+      ".local/share/direnv"
 #      {
 #        directory = ".local/share/Steam";
 #        method = "symlink";
 #      }
-#    ];
-#    files = [
-#      ".screenrc"
-#    ];
-#    allowOther = true;
-#  };
+    ];
+    files = [
+      ".screenrc"
+    ];
+    allowOther = true;
+  };
 
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
