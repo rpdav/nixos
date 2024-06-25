19379556e3017f6fb86f7ef72164210c8841acc4
ryan <105075689+rpdav@users.noreply.github.com>
2024-06-25 12:22:01 -0400

    reenabled persistance

--- configuration.nix
+++ configuration.nix
@@ -97,22 +97,22 @@
     geany
   ];
 
-#  environment.persistence."/persist" = {
-#    enable = true;  # NB: Defaults to true, not needed
-#    hideMounts = true;
-#    directories = [
+  environment.persistence."/persist" = {
+    enable = true;  # NB: Defaults to true, not needed
+    hideMounts = true;
+    directories = [
 #      "/var/log"
-#      "/var/lib/bluetooth"
-#      "/var/lib/nixos"
-#      "/var/lib/systemd/coredump"
-#      "/etc/NetworkManager/system-connections"
-#      { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
-#    ];
-#    files = [
-#      "/etc/machine-id"
-#      { file = "/var/keys/secret_file"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
-#    ];
-#  };
+      "/var/lib/bluetooth"
+      "/var/lib/nixos"
+      "/var/lib/systemd/coredump"
+      "/etc/NetworkManager/system-connections"
+      { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
+    ];
+    files = [
+      "/etc/machine-id"
+      { file = "/var/keys/secret_file"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
+    ];
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
