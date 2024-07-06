91e5f3bf12d6c6923ecff8a6cac663888b664284
ryan <105075689+rpdav@users.noreply.github.com>
2024-07-05 22:05:45 -0400

    prep for system wipe

--- configuration.nix
+++ configuration.nix
@@ -39,6 +39,11 @@
     };
   };
   
+  security.sudo.extraConfig = ''
+    # rollback results in sudo lectures after reboot
+    Defaults lecture = never
+    '';
+    
   networking.hostName = "nixbook";
   networking.networkmanager.enable = true;
 
@@ -60,6 +65,7 @@
 
 ## This may be replaced by home manager block.
   users.users.ryan = {
+    hashedPassword = "***REMOVED***";
     isNormalUser = true;
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
@@ -76,30 +82,37 @@
     wget
     git
     git-crypt
-    geany
   ];
 
 ## Persistence
+  environment.etc = {
+    nixos.source = "/persist/etc/nixos";
+    NIXOS.source = "/persist/etc/NIXOS";
+    adjtime.source = "/persist/etc/adjtime";
+  };
+
   environment.persistence."/persist" = {
     enable = true;  # NB: Defaults to true, not needed
     hideMounts = true;
     directories = [
-#      "/var/log"
+      "/var/log"
       "/var/lib/bluetooth"
       "/var/lib/nixos"
       "/var/lib/systemd/coredump"
       "/etc/NetworkManager/system-connections"
       { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
+#      "/etc/.updated" #systemd file with timestamp for when it was updated
+#      "/var/.updated" #systemd file with timestamp for when it was updated
+#      "/var/lib/sddm" # something to do with KDE
     ];
     files = [
-      ## disable machine-id linking until system wiping is set up
-      # "/etc/machine-id"
-      # { file = "/var/keys/secret_file"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
+       "/etc/machine-id"
+       { file = "/var/keys/secret_file"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
     ];
     ## User persistence is here since I couldn't get it to work in home-manager
     users.ryan = {
-      hideMounts = true;
       directories = [
+        ".config"
         "Desktop"
         "Downloads"
         "Music"
@@ -120,7 +133,7 @@
 #        };
       ];
       files = [
-        ".screenrc"
+        ".Xauthority"
       ];
     };
   };
