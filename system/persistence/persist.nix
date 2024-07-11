5ddfaaf1d9626bf7ec592092b25efcac4983374d
ryan <105075689+rpdav@users.noreply.github.com>
2024-07-11 18:33:32 -0400

    added wifi

--- system/persistence/persist.nix
+++ system/persistence/persist.nix
@@ -17,6 +17,7 @@
       "/var/lib/nixos"
       "/var/lib/systemd/coredump"
       "/etc/NetworkManager/system-connections"
+      "/var/lib/NetworkManager"
       { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
 #      "/etc/.updated" #systemd file with timestamp for when it was updated
 #      "/var/.updated" #systemd file with timestamp for when it was updated
