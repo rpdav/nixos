5ea7f441f8c226271263f86082ba4ac9910b1e59
ryan <105075689+rpdav@users.noreply.github.com>
2024-07-11 21:02:10 -0400

    add default browser

--- system/configuration.nix
+++ system/configuration.nix
@@ -90,6 +90,20 @@
     git-crypt
   ];
 
+## Default apps
+  xdg.mimeApps = {
+    enable = true;
+      defaultApplications = {
+        "default-web-browser" = [ "firefox.desktop" ];
+        "text/html" = [ "firefox.desktop" ];
+        "x-scheme-handler/http" = [ "firefox.desktop" ];
+        "x-scheme-handler/https" = [ "firefox.desktop" ];
+        "x-scheme-handler/about" = [ "firefox.desktop" ];
+        "x-scheme-handler/unknown" = [ "firefox.desktop" ];
+      };
+    };
+  };
+
 ## This should not be changed unless doing a fresh install
   system.stateVersion = "24.05"; # Did you read the comment?
 
