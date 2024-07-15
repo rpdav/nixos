8f6869f83e2873abc79cfcd3875038f38628152c
ryan <105075689+rpdav@users.noreply.github.com>
2024-07-15 08:35:42 -0400

    added borg test config

--- system/services/backup.nix
+++ system/services/backup.nix
@@ -1,10 +1,35 @@
-{config, pkgs, lib, secrets, ... }:
+{config, pkgs, lib, secrets, systemSettings, ... }:
 {
 
   fileSystems."/mnt/backup" = {
     device = "//10.10.1.17/secure/backups/nixbook";
     fsType = "cifs";
     options = [ "uid=ryan" "gid=users" "username=ryan" "password=${secrets.backup.password}" "noauto" "users" ];
+  };
+
+  services.borgbackup.jobs."nas-test" = {
+    paths = [ "/persist/home/ryan/Documents" ];
+    exclude = [
+      "/persist/home/ryan/OpenTTD"
+      "/persist/home/ryan/medical"
+    ];
+    user = "root";
+    repo = ("root@skynet-nas:/tank/backups" + ("/" + systemSettings.hostname + "/") + "./test");
+    doInit = true;
+    startAt = [ ]; #replace with daily or whatver. this is for manual running
+##    preHook = optional snapshotting/mounting command
+##    postHook = optional snapshot deletion/unmount
+    encryption = {
+      mode = "repokey-blake2";
+      passphrase = "${secrets.borg.testpassphrase}";
+    };
+    compression = "auto,lzma";
+    prune.keep = {
+      daily = 7;
+      weekly = 4;
+      monthly = 12;
+      yearly = 1;
+    };
   };
 
 }
