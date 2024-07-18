97003af3b5fd528fd3e81e0075f77112ab2b9638
ryan <105075689+rpdav@users.noreply.github.com>
2024-07-17 21:58:48 -0400

    Generation 69

--- user/app/browser/webapps.nix
+++ user/app/browser/webapps.nix
@@ -3,17 +3,17 @@
 {
   xdg.desktopEntries.actual = {
     name = "Actual";
-    exec = "${userSettings.browser} --app=https://budget.${secrets.selfhosting.domain}";
+    exec = "chromium --app=https://budget.${secrets.selfhosting.domain}";
   };
 
   xdg.desktopEntries.silverbullet= {
     name = "SilverBullet";
-    exec = "${userSettings.browser} --app=https://notes.${secrets.selfhosting.domain}";
+    exec = "chromium --app=https://notes.${secrets.selfhosting.domain}";
   };
 
   xdg.desktopEntries.homeassistant= {
     name = "Home Assistant";
-    exec = "${userSettings.browser} --app=https://home.${secrets.selfhosting.domain}";
+    exec = "chromium --app=https://home.${secrets.selfhosting.domain}";
   };
 
   xdg.desktopEntries.nirvana= {
