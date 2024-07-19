a23bf8510d452d5bca55d2aab8e28095f7e4a57a
ryan <105075689+rpdav@users.noreply.github.com>
2024-07-19 14:47:50 -0400

    added comment on imperative proton setup

--- user/app/games/proton.nix
+++ user/app/games/proton.nix
@@ -5,6 +5,7 @@
     protonup
   ];
 
+## This must be imperatively set up by running "protonup". Further updates are handled by steam
   home.sessionVariables = {
     STEAM_EXTRA_COMPAT_TOOLS_PATHS =
       "/home/${userSettings.username}/.steam/root/compatibilitytools.d";
