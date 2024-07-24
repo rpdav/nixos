b128492a7dad0f6d0921abd0b8b4faa8ed6ceb11
ryan <105075689+rpdav@users.noreply.github.com>
2024-07-24 12:54:21 -0400

    added commented section to exclude packages, disabled x11

--- system/wm/kde.nix
+++ system/wm/kde.nix
@@ -1,10 +1,18 @@
 { config, pkgs, ... }:
 
 {
-  services.xserver.enable = true;
+#  services.xserver.enable = true;
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
   };
   services.desktopManager.plasma6.enable = true;
+
+## Disable packegs if desired
+#  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+#    plasma-browser-integration
+#    konsole
+#    oxygen
+#  ];
+
 }
