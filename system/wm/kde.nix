38fe2daa69150aba813770caeb8a344515391736
ryan <105075689+rpdav@users.noreply.github.com>
2024-07-29 19:02:29 -0400

    excluded redundant pkg

--- system/wm/kde.nix
+++ system/wm/kde.nix
@@ -3,18 +3,17 @@
 
 {
 
-#  services.xserver.enable = true;
+  #services.xserver.enable = true;
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
   };
   services.desktopManager.plasma6.enable = true;
 
-## Disable packages if desired
-#  environment.plasma6.excludePackages = with pkgs.kdePackages; [
-#    plasma-browser-integration
-#    konsole
-#    oxygen
-#  ];
+## Disable packages
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    plasma-browser-integration
+    firefox # already in home.nix
+  ];
 
 }
