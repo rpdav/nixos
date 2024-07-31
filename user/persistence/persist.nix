61d9f9fe968fb77bfb1370a41013e3821d4864e2
ryan <105075689+rpdav@users.noreply.github.com>
2024-07-31 10:48:03 -0400

    brackets

--- user/persistence/persist.nix
+++ user/persistence/persist.nix
@@ -1,11 +1,10 @@
 { config, userSettings, impermanence, ... }:
 
 {
-  {
+  
   home.persistence."/persistent/home/${userSettings.username}" = {
     directories = [
       "testdir1"
-      }
     ];
     files = [
       "testfile1"
