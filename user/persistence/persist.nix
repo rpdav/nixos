bbef9ba0c06c9f45c5a7cd3aba9b0e2592d750f7
ryan <105075689+rpdav@users.noreply.github.com>
2024-07-31 10:44:54 -0400

    testing again

--- user/persistence/persist.nix
+++ user/persistence/persist.nix
@@ -1,6 +1,17 @@
 { config, userSettings, impermanence, ... }:
 
 {
-  
+  {
+  home.persistence."/persistent/home/${userSettings.username}" = {
+    directories = [
+      "testdir1"
+      }
+    ];
+    files = [
+      "testfile1"
+    ];
+    allowOther = true;
+  };
+}
 
 }
