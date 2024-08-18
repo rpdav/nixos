8338b1534c2909d92f7119e14c7b584167f7eb82
ryan <105075689+rpdav@users.noreply.github.com>
2024-08-18 19:29:01 -0400

    added failure notifications and network start delay from wiki

--- modules/nixos/localbackup.nix
+++ modules/nixos/localbackup.nix
@@ -1,5 +1,45 @@
 {config, pkgs, lib, secrets, userSettings, ... }:
-{
+
+let
+## Set up notifications in case of failure
+  borgbackupMonitor = { config, pkgs, lib, ... }: with lib; {
+    key = "borgbackupMonitor";
+    _file = "borgbackupMonitor";
+    config.systemd.services = {
+      "notify-problems@" = {
+        enable = true;
+        serviceConfig.User = "danbst";
+        environment.SERVICE = "%i";
+        script = ''
+          export $(cat /proc/$(${pkgs.procps}/bin/pgrep "gnome-session" -u "$USER")/environ |grep -z '^DBUS_SESSION_BUS_ADDRESS=')
+          ${pkgs.libnotify}/bin/notify-send -u critical "$SERVICE FAILED!" "Run journalctl -u $SERVICE for details"
+        '';
+      };
+    } // flip mapAttrs' config.services.borgbackup.jobs (name: value:
+      nameValuePair "borgbackup-job-${name}" {
+        unitConfig.OnFailure = "notify-problems@%i.service";
+      ## Wait for network access
+        preStart = lib.mkBefore ''
+          # waiting for internet after resume-from-suspend
+          until /run/wrappers/bin/ping google.com -c1 -q >/dev/null; do :; done
+        '';
+      }
+    );
+
+    # optional, but this actually forces backup after boot in case laptop was powered off during scheduled event
+    # for example, if you scheduled backups daily, your laptop should be powered on at 00:00
+    config.systemd.timers = flip mapAttrs' config.services.borgbackup.jobs (name: value:
+      nameValuePair "borgbackup-job-${name}" {
+        timerConfig.Persistent = lib.mkForce true;
+      }
+    );
+  };
+
+in {
+  imports =
+    [
+      borgbackupMonitor
+    ];
 
 # This config assumes this machine's root user public key is copied to the borg server as /sshkeys/clients/$hostname. The server will create a backup directory under /backup/$hostname
 
