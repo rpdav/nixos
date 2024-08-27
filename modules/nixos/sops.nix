a69ee6a0e3459e6cdd0e303d42714059387e63c5
ryan <105075689+rpdav@users.noreply.github.com>
2024-08-26 20:57:30 -0400

    More sops config

--- modules/nixos/sops.nix
+++ modules/nixos/sops.nix
@@ -6,8 +6,9 @@
     defaultSopsFile = ../../secrets/secrets.yaml;   
     defaultSopsFormat = "yaml";
     age = {
-      # automatically import ssh keys as age keys
-      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+      # Automatically import ssh keys as age keys
+      # Must be in persistent volume if using impermanence
+      sshKeyPaths = [ "/persist/etc/ssh/ssh_host_ed25519_key" ];
       # specified location for age key
       keyFile = "/var/lib/sops-nix/key.txt";
       # generate age key from ssh key if not already present
