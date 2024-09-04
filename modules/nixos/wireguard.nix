f2631de2f63de7925d597ec790fb2cd8489850ae
ryan <105075689+rpdav@users.noreply.github.com>
2024-09-04 09:58:01 -0400

    added vps config

--- modules/nixos/wireguard.nix
+++ modules/nixos/wireguard.nix
@@ -2,6 +2,7 @@
 {
   sops.secrets = {
     "wireguard/home/privateKey" = { };
+    "wireguard/vps/privateKey" = { };
   };
 
   networking.wg-quick.interfaces = {
@@ -14,6 +15,17 @@
           publicKey = "gy5zJPgMgC5ZBLScN7Wqiu0KXNL+rYGAnYs7c7wS72g=";
           allowedIPs = [ "10.10.10.0/32" "10.10.1.0/24" "10.10.30.0/24" ];
           endpoint = "${secrets.wireguard.home.endpoint}:51820";
+        }
+      ];
+    };
+    vps = {
+      address = [ "10.100.94.11/32" ];
+      privateKeyFile = "${config.sops.secrets."wireguard/vps/privateKey".path}";
+      peers = [
+        { 
+          publicKey = "K+ROdMPjS4eUA804orSk9Vl8NryfvaiWUfPzqBEFbVw=";
+          allowedIPs = [ "10.36.7.11/32" "10.100.94.0/24" ];
+          endpoint = "${secrets.wireguard.vps.endpoint}:51820";
         }
       ];
     };
