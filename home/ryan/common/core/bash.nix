496cfc6b9f808d25e72a56f2d5c1ceb0172f3830
ryan <105075689+rpdav@users.noreply.github.com>
2024-11-16 19:14:21 +0000

    disabled starship - needed troubleshooting

--- home/ryan/common/core/bash.nix
+++ home/ryan/common/core/bash.nix
@@ -15,19 +15,19 @@
       gc = "git commit -am";
       gp = "git push";
     };
-#    bashrcExtra = ''
-#      force_color_prompt=yes
-#      PS1='\[\033[1;36m\]\u\[\033[1;31m\]@\[\033[1;32m\]\h:\[\033[1;35m\]\w\[\033[1;31m\]\$\[\033[0m\] '
-#    '';
+    bashrcExtra = ''
+      force_color_prompt=yes
+      PS1='\[\033[1;36m\]\u\[\033[1;31m\]@\[\033[1;32m\]\h:\[\033[1;35m\]\w\[\033[1;31m\]\$\[\033[0m\] '
+    '';
   };
 
   programs.autojump.enable = true;
 
-  programs.starship = {
-    enable = true;
-    settings = {
-      settings = pkgs.lib.importTOML ./starship.toml;
-    };
-  };
+#  programs.starship = {
+#    enable = true;
+#    settings = {
+#      settings = pkgs.lib.importTOML ./starship.toml;
+#    };
+#  };
 
 }
