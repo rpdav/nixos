609903f649e80d654b7f707f73c0f9c3cf245906
ryan <105075689+rpdav@users.noreply.github.com>
2024-11-20 17:36:00 -0500

    enabled passwordless sudo for remote connections

--- hosts/common/core/sshd.nix
+++ hosts/common/core/sshd.nix
@@ -1,16 +1,31 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
+  
   services.openssh = {
     enable = true;
     ports = [ 22 ];
     settings.PasswordAuthentication = false;
     hostKeys = [
       {
-        comment = "key comment";
+        comment = "server key";
         path = "/etc/ssh/ssh_host_ed25519_key";
         rounds = 100;
         type = "ed25519";
       }
     ];
   };
+
+  # Enable remote passwordless sudo
+  security.pam.services.sudo =
+    { config, ... }:
+    {
+      rules.auth.rssh = {
+        order = config.rules.auth.ssh_agent_auth.order - 1;
+        control = "sufficient";
+        modulePath = "${pkgs.pam_rssh}/lib/libpam_rssh.so";
+        settings.authorized_keys_command = pkgs.writeShellScript "get-authorized-keys" ''
+          cat "/etc/ssh/authorized_keys.d/$1"
+        '';
+      }; 
+    };
 }
