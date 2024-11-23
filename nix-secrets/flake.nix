88db7a552f16885f41bd9baf3e84c9c747cda586
ryan <105075689+rpdav@users.noreply.github.com>
2024-11-23 13:48:52 -0500

    updated documentation, removed deprecated json secrets

---
+++ nix-secrets/flake.nix
@@ -0,0 +1,24 @@
+{
+  outputs = {...}:
+    {
+      personal-mail = {
+        address = "";
+        realName = "";
+      };
+      admin-mail = {
+        host = "";
+        address = "";
+        realName = "";
+      };
+      calendar = {
+        url = "";
+        user = "";
+        password = "";
+      };
+      wireguard = {
+        home.endpoint = "";
+        vps.endpoint = "";
+      };
+      selfhosting.startpage = "";
+    };
+}
