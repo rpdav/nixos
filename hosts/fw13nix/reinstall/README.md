bfa955f80cda30ba8af400570dae226b6c69213f
ryan <105075689+rpdav@users.noreply.github.com>
2024-11-29 09:10:20 -0500

    initial documentation draft, added sshd for age keygen

--- hosts/fw13nix/reinstall/README.md
+++ hosts/fw13nix/reinstall/README.md
@@ -1,1 +1,15 @@
 WIP
+
+Rough plan:
+1. Boot into installer
+2. Pull hardware config w/o filesystems
+3. Partition with disko (maybe using disk config w/o custom options)
+4. Install
+5. OR instead of 3 and 4, install directly using disko-install
+6. Reboot into new OS
+7. Restore persistent data using temporary ssh key
+8. For brand new host:
+    8a. Generate new sshd keys
+    8b. Pull and update sops repo with sshd keys (need to copy user age key to ~/.config/sops)
+    8c. Push sops repo and update flake input
+9. Update config with new hardware-configuration.nix and rebuild
--- hosts/fw13nix/reinstall/configuration.nix
+++ hosts/fw13nix/reinstall/configuration.nix
@@ -3,15 +3,15 @@
 
   imports = [ ./disko.nix ];
 
-## Enable flakes
+  # Enable flakes
   nix = {
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
   };
 
-## Enable support for encrypted partition
-## Remove any other auto-generated boot.loader lines
+  # Enable support for encrypted partition
+  # Remove any other auto-generated boot.loader lines
   #TODO: replace this with systemdboot
   boot = {
     loader = {
@@ -27,17 +27,31 @@
     };
     initrd.luks.devices = {
       crypt = {
-                 ## UUID of the encrypted partition 
-        device = "/dev/disk/by-uuid/xxxxx";
+        device = "/dev/disk/by-uuid/xxxxx"; # UUID of the encrypted partition 
         preLVM = true;
       };
     };
   };
 
-## Timezone
+  # Create ssh host keys to import into sops if they don't already exist in backup
+  services.openssh = {
+    enable = true;
+    ports = [22];
+    settings.PasswordAuthentication = false;
+    hostKeys = [
+      {
+        comment = "server key";
+        path = "/etc/ssh/ssh_host_ed25519_key";
+        rounds = 100;
+        type = "ed25519";
+      }
+    ];
+  };
+
+  # Timezone
   time.timeZone = "America/Indiana/Indianapolis";
 
-## Useful packages for intial reinstall
+  # Useful packages for intial reinstall
   environment.systemPackages = with pkgs; [
     firefox
     git
@@ -48,20 +62,21 @@
     ssh-to-age
   ];
 
-## Define primary user
+  # Define primary user
   users.users.yourname = {
-    hashedPassword = "Run mkpasswd -m sha-512 to generate";
+    # the hash below is for the password `changeme` - obviously only use this for this bare-bones installl config
+    hashedPassword = "$6$7y9RbBEMGo1Fx.Pr$rM1PReeNvbKM1QCQvrNJ5BAYY3SlYDr49MTT0j6wv7cz5p0ezPz8ddihkyutowzEie1.NGFxzSpfawY0s9L2q1";
     isNormalUser = true; 
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
   };
 
-## Enable gnome on xserver
+  # Enable gnome on xserver
   services.xserver = {
     enable = true;
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
   };
 
-## This should not be changed unless doing a fresh install
-  system.stateVersion = "24.05"; # Did you read the comment?
+  # Change this to match installer version
+  system.stateVersion = "24.05";
 }
