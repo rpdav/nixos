9ac24cc17361b62160047328a4ec88f55296d4fb
ryan <105075689+rpdav@users.noreply.github.com>
2024-12-01 14:35:51 -0500

    fixed nextcloud auto-start

--- home/ryan/fw13.nix
+++ home/ryan/fw13.nix
@@ -69,6 +69,11 @@
     enable = true;
     startInBackground = true;
   };
-
+  # client starts to early and fails; this delays it a bit
+  systemd.user.services.nextcloud-client = {
+    Unit = {
+      After = pkgs.lib.mkForce "graphical-session.target"; 
+    };
+  };
 }
 
