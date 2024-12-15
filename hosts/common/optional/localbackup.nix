0c3e9335891eb994f6ef1945892da1d4950e582d
ryan <105075689+rpdav@users.noreply.github.com>
2024-12-15 17:54:42 -0500

    added root user config for borg

--- hosts/common/optional/localbackup.nix
+++ hosts/common/optional/localbackup.nix
@@ -4,64 +4,70 @@
   lib,
   userSettings,
   ...
-}: let
-  ## Set up notifications in case of failure
-  borgbackupMonitor = {
-    config,
-    pkgs,
-    lib,
-    ...
-  }:
-    with lib; {
-      key = "borgbackupMonitor";
-      _file = "borgbackupMonitor";
-      config.systemd.services =
-        {
-          "notify-problems@" = {
-            enable = true;
-            serviceConfig.User = "danbst";
-            environment.SERVICE = "%i";
-            script = ''
-              export $(cat /proc/$(${pkgs.procps}/bin/pgrep "gnome-session" -u "$USER")/environ |grep -z '^DBUS_SESSION_BUS_ADDRESS=')
-              ${pkgs.libnotify}/bin/notify-send -u critical "$SERVICE FAILED!" "Run journalctl -u $SERVICE for details"
-            '';
-          };
-        }
-        // flip mapAttrs' config.services.borgbackup.jobs (
-          name: value:
-            nameValuePair "borgbackup-job-${name}" {
-              unitConfig.OnFailure = "notify-problems@%i.service";
-              ## Wait for network access
-              preStart = lib.mkBefore ''
-                # waiting for internet after resume-from-suspend
-                until /run/wrappers/bin/ping google.com -c1 -q >/dev/null; do :; done
-              '';
-            }
-        );
-
-      # optional, but this actually forces backup after boot in case laptop was powered off during scheduled event
-      # for example, if you scheduled backups daily, your laptop should be powered on at 00:00
-      config.systemd.timers = flip mapAttrs' config.services.borgbackup.jobs (
-        name: value:
-          nameValuePair "borgbackup-job-${name}" {
-            timerConfig.Persistent = lib.mkForce true;
-          }
-      );
-    };
-in {
-  imports = [
-    borgbackupMonitor
-  ];
+}: 
+#let
+#  ## Set up notifications in case of failure
+#  borgbackupMonitor = {
+#    config,
+#    pkgs,
+#    lib,
+#    ...
+#  }:
+#    with lib; {
+#      key = "borgbackupMonitor";
+#      _file = "borgbackupMonitor";
+#      config.systemd.services =
+#        {
+#          "notify-problems@" = {
+#            enable = true;
+#            serviceConfig.User = "danbst";
+#            environment.SERVICE = "%i";
+#            script = ''
+#              export $(cat /proc/$(${pkgs.procps}/bin/pgrep "gnome-session" -u "$USER")/environ |grep -z '^DBUS_SESSION_BUS_ADDRESS=')
+#              ${pkgs.libnotify}/bin/notify-send -u critical "$SERVICE FAILED!" "Run journalctl -u $SERVICE for details"
+#            '';
+#          };
+#        }
+#        // flip mapAttrs' config.services.borgbackup.jobs (
+#          name: value:
+#            nameValuePair "borgbackup-job-${name}" {
+#              unitConfig.OnFailure = "notify-problems@%i.service";
+#              ## Wait for network access
+#              preStart = lib.mkBefore ''
+#                # waiting for internet after resume-from-suspend
+#                until /run/wrappers/bin/ping google.com -c1 -q >/dev/null; do :; done
+#              '';
+#            }
+#        );
+#
+#      # optional, but this actually forces backup after boot in case laptop was powered off during scheduled event
+#      # for example, if you scheduled backups daily, your laptop should be powered on at 00:00
+#      config.systemd.timers = flip mapAttrs' config.services.borgbackup.jobs (
+#        name: value:
+#          nameValuePair "borgbackup-job-${name}" {
+#            timerConfig.Persistent = lib.mkForce true;
+#          }
+#      );
+#    };
+#in 
+{
+#  imports = [
+#    borgbackupMonitor
+#  ];
 
   # This config assumes this machine's root user public key is copied to the borg server as /sshkeys/clients/$hostname. The server will create a backup directory under /backup/$hostname
 
   ## Local backup definition
-  sops.secrets."borg/passphrase" = {};
+  sops.secrets = {
+    "borg/passphrase" = {};
+    "${userSettings.username}/sshKeys/id_borg".path = "/root/.ssh/id_ed25519";
+  };
 
   services.borgbackup.jobs."local" = {
     paths = ["/persist"];
     exclude = [
       # Run `borg help patterns` for guidance on exclusion patterns
+      "/persist/home/*/.git/**" #can be restored from repo
       "/persist/*/.thunderbird/*/ImapMail" #email doesn't need backup
       "/persist/home/*/Nextcloud" #already on server
       "/persist/home/*/.local/share/Steam" #lots of small files and big games
