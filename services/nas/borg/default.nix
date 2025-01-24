a64d1373756300755708e2a4c81ec61c1cd8d3eb
ryan <105075689+rpdav@users.noreply.github.com>
2025-01-23 22:03:53 -0500

    fixed borg volume

--- services/nas/borg/default.nix
+++ services/nas/borg/default.nix
@@ -4,8 +4,8 @@
   # Create directories for appdata
   # d to create the directory, Z to recursively correct ownership (only needed when restoring from backup)
   systemd.tmpfiles.rules = [
-    "d ${serviceOpts.dockerDir}/borgserver/config 0700 ${serviceOpts.dockerUser} users"
-    "Z ${serviceOpts.dockerDir}/borgserver/config - ${serviceOpts.dockerUser} users"
+    "d ${serviceOpts.dockerDir}/borg/sshkeys 0700 ${serviceOpts.dockerUser} users"
+    "Z ${serviceOpts.dockerDir}/borg/sshkeys - ${serviceOpts.dockerUser} users"
   ];
 
 }
