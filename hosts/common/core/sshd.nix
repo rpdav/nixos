6194364debca12642bda25cd5eeaf9d1c5cae1c4
ryan <105075689+rpdav@users.noreply.github.com>
2025-04-03 21:08:21 -0400

    formatting

--- hosts/common/core/sshd.nix
+++ hosts/common/core/sshd.nix
@@ -28,40 +28,40 @@
   };
 
   programs.ssh.extraConfig = ''
-      Host nas
-        HostName 10.10.1.17
-       	User ryan
-       	Port 22
+    Host nas
+      HostName 10.10.1.17
+      User ryan
+      Port 22
 
-      Host pi
-       	HostName 10.10.1.10
-       	User pi
-       	Port 22
+    Host pi
+      HostName 10.10.1.10
+      User pi
+      Port 22
 
-      Host vps
-	Hostname 170.187.148.177
-        User ryan
+    Host vps
+      Hostname 172.233.209.173
+      User ryan
 
-      Host borg
-        Hostname 10.10.1.17
-	Port 2222
-        User borg
+    Host borg
+      Hostname 10.10.1.17
+      Port 2222
+      User borg
 
-      Host testbox
-	Hostname 10.10.1.18
-	User ryan
+    Host testbox
+      Hostname 10.10.1.18
+      User ryan
 
-      Host testvm
-	Hostname 10.10.1.19
-	User ryan
+    Host testvm
+      Hostname 10.10.1.19
+      User ryan
 
-      Host gitea.dfrp.xyz
-        User git
-	Port 2223
+    Host gitea.dfrp.xyz
+      User git
+      Port 2223
 
-      Host github.com
-	User git
-    '';
+    Host github.com
+      User git
+  '';
 
   # Enable remote passwordless sudo
   security.pam.services.sudo = {config, ...}: {
