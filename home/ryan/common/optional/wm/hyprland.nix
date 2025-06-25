1f35d44e2fc7c914e83294b15f9e2b5ba3575f55
ryan <105075689+rpdav@users.noreply.github.com>
2025-06-25 13:22:06 -0400

    added input group for waybar

--- home/ryan/common/optional/wm/hyprland.nix
+++ home/ryan/common/optional/wm/hyprland.nix
@@ -1,16 +1,25 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  systemOpts,
+  userOpts,
+  lib,
+  ...
+}: {
   home.packages = with pkgs; [
     kdePackages.dolphin
+    brightnessctl
   ];
 
   # launcher
-  programs.rofi = {
+  programs.fuzzel = {
     enable = true;
-    terminal = "${pkgs.kitty}/bin/kitty";
-    modes = [
-      "drun"
-      "ssh"
-    ];
+    settings = {
+      main = {
+        #TODO: integrate this with stylix?
+        font = lib.mkForce "monospace:size=14";
+        dpi-aware = "yes";
+      };
+    };
   };
 
   #waybar
@@ -19,46 +28,94 @@
   };
 
   #notifications
-
-  wayland.windowManager.hyprland = {
+  services.dunst = {
     enable = true;
-    plugins = [];
-    settings = {
-      ################
-      ### MONITORS ###
-      ################
-
-      ###################
-      ### MY PROGRAMS ###
-      ###################
-
-      "$terminal" = "${pkgs.kitty}/bin/kitty";
-      "$fileManager" = "${pkgs.kdePackages.dolphin}/bin/dolphin";
-      "$menu" = "${pkgs.rofi}/bin/rofi";
-
-      #################
-      ### AUTOSTART ###
-      #################
-
-      exec-once = [
-        "${pkgs.waybar}/bin/waybar"
-      ];
-
-      ###################
-      ### KEYBINDINGS ###
-      ###################
-
-      "$mainMod" = "SUPER";
-      bind = [
-        "$mainMod, Q, exec, $terminal"
-        "$mainMod, C, killactive,"
-        "$mainMod, M, exit,"
-        "$mainMod, E, exec, $fileManager"
-        "$mainMod, V, togglefloating,"
-        "$mainMod, R, exec, $menu -show drun"
-        "$mainMod, P, pseudo," # dwindle
-        "$mainMod, J, togglesplit," # dwindle
-      ];
-    };
   };
+
+  # Stylix overrides for hyprland
+
+  stylix.opacity.terminal = lib.mkForce 1.0;
+
+  # Temporary - will iterate on hyprland.conf then convert it to nix when done
+  home.persistence."${systemOpts.persistVol}/home/${userOpts.username}" = lib.mkIf systemOpts.impermanent {
+    directories = [
+      ".config/hypr"
+    ];
+  };
+
+  #  wayland.windowManager.hyprland = {
+  #    enable = true;
+  #    plugins = [];
+  #    settings = {
+  #      ################
+  #      ### MONITORS ###
+  #      ################
+  #
+  #      ###################
+  #      ### MY PROGRAMS ###
+  #      ###################
+  #
+  #      "$terminal" = "${pkgs.kitty}/bin/kitty";
+  #      "$fileManager" = "${pkgs.kdePackages.dolphin}/bin/dolphin";
+  #      "$menu" = "${pkgs.rofi}/bin/rofi";
+  #      "$browser" = "${pkgs.firefox}/bin/firefox";
+  #
+  #      #################
+  #      ### AUTOSTART ###
+  #      #################
+  #
+  #      exec-once = [
+  #        "${pkgs.waybar}/bin/waybar"
+  #      ];
+  #
+  #      ###################
+  #      ### KEYBINDINGS ###
+  #      ###################
+  #
+  #      "$mainMod" = "SUPER";
+  #      bind =
+  #        [
+  #          "$mainMod, Q, exec, $terminal"
+  #          "$mainMod, C, killactive,"
+  #          "$mainMod, M, exit,"
+  #          "$mainMod, E, exec, $fileManager"
+  #          "$mainMod, B, exec, $browser"
+  #          "$mainMod, V, togglefloating,"
+  #          "$mainMod, R, exec, $menu -show drun"
+  #          "$mainMod, P, pseudo," # dwindle
+  #          "$mainMod, J, togglesplit," # dwindle
+  #
+  #          "$mainMod, left, movefocus, l"
+  #          "$mainMod, right, movefocus, r"
+  #          "$mainMod, up, movefocus, u"
+  #          "$mainMod, down, movefocus, d"
+  #        ]
+  #        ++ (
+  #          # workspaces
+  #          # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+  #          builtins.concatLists (builtins.genList (
+  #              i: let
+  #                ws = i + 1;
+  #              in [
+  #                "$mainMod, code:1${toString i}, workspace, ${toString ws}"
+  #                "$mainMod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+  #              ]
+  #            )
+  #            9)
+  #        );
+  #      bindel = [
+  #        ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+  #        ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+  #        ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+  #        ",XF86MonBrightnessUp, exec, brightnessctl -e4 -n2 set 5%+"
+  #        ",XF86MonBrightnessDown, exec, brightnessctl -e4 -n2 set 5%-"
+  #      ];
+  #
+  #      ############
+  #      ### MISC ###
+  #      ############
+  #
+  #      input.touchpad.natural_scroll = true;
+  #    };
+  #  };
 }
--- hosts/common/optional/wm/hyprland.nix
+++ hosts/common/optional/wm/hyprland.nix
@@ -1,11 +1,16 @@
 {
   inputs,
   pkgs,
+  userOpts,
   ...
 }: {
   nix.settings = {
     substituters = ["https://hyprland.cachix.org"];
     trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
+
+  users.users.${userOpts.username} = {
+    extraGroups = ["input"];
   };
 
   programs.hyprland = {
