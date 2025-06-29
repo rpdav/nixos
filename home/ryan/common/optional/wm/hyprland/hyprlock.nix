51d03b5120d321e75acbf0e7f95db930e7dee49b
ryan <105075689+rpdav@users.noreply.github.com>
2025-06-29 19:09:46 -0400

    justfile tweaks, removed hyprlock temp code

--- home/ryan/common/optional/wm/hyprland/hyprlock.nix
+++ home/ryan/common/optional/wm/hyprland/hyprlock.nix
@@ -1,15 +1,4 @@
-{
-  pkgs,
-  lib,
-  ...
-}: {
-  #  # Temporary config
-  #  home.packages = with pkgs; [
-  #    hyprlock
-  #  ];
-  #
-  #  home.file.".config/hypr/hyprlock.conf".source = config.lib.file.mkOutOfStoreSymlink "/home/ryan/nixos/home/ryan/common/optional/wm/hyprland/hyprlock.conf";
-
+{lib, ...}: {
   programs.hyprlock = {
     enable = true;
     settings = {
