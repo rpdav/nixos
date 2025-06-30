34b0cf951a22faf359d98b4b9236b99141c20240
ryan <105075689+rpdav@users.noreply.github.com>
2025-06-30 16:17:48 -0400

    refactor

--- home/ryan/common/optional/wm/hyprland/hypridle.nix
+++ home/ryan/common/optional/wm/hyprland/hypridle.nix
@@ -6,35 +6,39 @@
 }: {
   services.hypridle = {
     enable = true;
-    settings = {
+    settings = let
+      lock_cmd = "${pkgs.procps}/bin/pidof ${pkgs.hyprlock}/bin/hyprlock || ${pkgs.hyprlock}/bin/hyprlock"; # Avoid starting multiple hyprlock instances.
+      hyprctl = "${inputs.hyprland.packages.${pkgs.system}.hyprland}/bin/hyprctl"; # Use hyprland flake for hyprctl
+      brightnessctl = "${pkgs.brightnessctl}/bin/brightnessctl";
+    in {
       general = {
-        lock_cmd = "${pkgs.procps}/bin/pidof ${pkgs.hyprlock}/bin/hyprlock || ${pkgs.hyprlock}/bin/hyprlock"; # avoid starting multiple hyprlock instances.
-        after_sleep_cmd = "hyprctl dispatch dpms on";
-        ignore_dbus_inhibit = false;
+        inherit lock_cmd;
+        before_sleep_cmd = lock_cmd;
+        after_sleep_cmd = "${hyprctl} dispatch dpms on"; # turn on screen after sleep
       };
       listener = [
         # screen dim
         {
           timeout = config.systemOpts.screenDimTimeout;
-          on-timeout = "${pkgs.brightnessctl}/bin/brightnessctl -s set 10";
-          on-resume = "${pkgs.brightnessctl}/bin/brightnessctl -r";
+          on-timeout = "${brightnessctl} -s set 10";
+          on-resume = "${brightnessctl} -r";
         }
         # keyboard dim
         {
           timeout = config.systemOpts.screenDimTimeout;
-          on-timeout = "i${pkgs.brightnessctl}/bin/brightnessctl -sd framework_laptop::kbd_backlight set 0";
-          on-resume = "${pkgs.brightnessctl}/bin/brightnessctl -rd framework_laptop::kbd_backlight";
+          on-timeout = "${brightnessctl} -sd framework_laptop::kbd_backlight set 0";
+          on-resume = "${brightnessctl} -rd framework_laptop::kbd_backlight";
         }
         # screen lock
         {
           timeout = config.systemOpts.lockTimeout;
-          on-timeout = "${pkgs.hyprlock}/bin/hyprlock";
+          on-timeout = lock_cmd;
         }
         # screen off after lock
         {
           timeout = config.systemOpts.screenOffTimeout;
-          on-timeout = "${inputs.hyprland.packages."${pkgs.system}".hyprland}/bin/hyprctl dispatch dpms off"; # use hyprctl from hyprland flake
-          on-resume = "${inputs.hyprland.packages."${pkgs.system}".hyprland}/bin/hyprctl dispatch dpms on && ${pkgs.brightnessctl}/bin/brightnessctl -r";
+          on-timeout = "${hyprctl} dispatch dpms off";
+          on-resume = "${hyprctl} dispatch dpms on && ${brightnessctl} -r";
         }
         # sleep
         {
