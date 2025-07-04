d6dd874b4fba639edfc0adc3f9a9a492292311a7
ryan <105075689+rpdav@users.noreply.github.com>
2025-07-04 17:07:08 -0400

    webapp refactor - initial config

--- home/ryan/common/optional/app/web-apps/default.nix
+++ home/ryan/common/optional/app/web-apps/default.nix
@@ -1,142 +1,78 @@
 {
-  userOpts,
   secrets,
+  configLib,
   ...
 }: {
-  # The StartumpWMClass must match this format in order to group windows properly. I'm not sure why this format in particular
-  # Pulled from repo https://github.com/bashfulrobot/nixos/tree/65bb54076ef00f08a1289a96425d68bf5e1f4036/modules/apps/web-apps
-
-  # If this gets unwieldy, consider making a webapp module like https://github.com/TLATER/dotfiles/blob/master/home-modules/firefox-webapp.nix
-
-  # Desktop file definitions
-  xdg.desktopEntries = {
+  imports = [(configLib.relativeToRoot "./modules/home-manager/web-app.nix")];
+  programs.firefox.webapps = let
+    domain = secrets.selfhosting.domain;
+  in {
     actual = {
+      enable = true;
+      id = 1;
+      url = "https://budget.${domain}";
       name = "Actual Budget";
-      comment = "Zero-base budgeting app";
-      settings.StartupWMClass = "chrome-budget.${secrets.selfhosting.domain}__-Default";
-      exec = "chromium --ozone-platform-hint=auto --force-dark-mode --enable-features=WebUIDarkMode --app=\"https://budget.${secrets.selfhosting.domain}\" %U";
-      icon = "/home/${userOpts.username}/.local/share/icons/actual.png";
-      terminal = false;
-      categories = ["Application"];
-      type = "Application";
+      genericName = "Budgeting Application";
+      icon = ./icons/actual.png;
+      categories = [
+        "Office"
+      ];
     };
-
     nextcloud = {
+      enable = true;
+      id = 2;
+      url = "https://cloud.${domain}";
       name = "Nextcloud";
-      comment = "Self-hosted cloud";
-      settings.StartupWMClass = "chrome-cloud.${secrets.selfhosting.domain}__-Default";
-      exec = "chromium --ozone-platform-hint=auto --force-dark-mode --enable-features=WebUIDarkMode --app=\"https://cloud.${secrets.selfhosting.domain}\" %U";
-      icon = "/home/${userOpts.username}/.local/share/icons/nextcloud.png";
-      terminal = false;
-      categories = ["Application"];
-      type = "Application";
+      genericName = "Cloud Synchronization";
+      icon = ./icons/nextcloud.png;
+      categories = [
+        "Network"
+        "Office"
+      ];
     };
-
-    silverbullet = {
-      name = "SilverBullet";
-      comment = "Markdown notes app";
-      settings.StartupWMClass = "chrome-notes.${secrets.selfhosting.domain}__-Default";
-      settings.Keywords = "notes";
-      exec = "chromium --ozone-platform-hint=auto --force-dark-mode --enable-features=WebUIDarkMode --app=\"https://notes.${secrets.selfhosting.domain}\" %U";
-      icon = "/home/${userOpts.username}/.local/share/icons/silverbullet.png";
-      terminal = false;
-      categories = ["Application"];
-      type = "Application";
-    };
-
     home-assistant = {
+      enable = true;
+      id = 3;
+      url = "https://home.${domain}";
       name = "Home Assistant";
-      comment = "Smart home automation";
-      settings.StartupWMClass = "chrome-home.${secrets.selfhosting.domain}__-Default";
-      exec = "chromium --ozone-platform-hint=auto --force-dark-mode --enable-features=WebUIDarkMode --app=\"https://home.${secrets.selfhosting.domain}\" %U";
-      icon = "/home/${userOpts.username}/.local/share/icons/home-assistant.png";
-      terminal = false;
-      categories = ["Application"];
-      type = "Application";
+      genericName = "Home Automation";
+      icon = ./icons/home-assistant.png;
+      categories = [
+        "Network"
+      ];
     };
-
     planka = {
+      enable = true;
+      id = 4;
+      url = "https://projects.${domain}";
       name = "Planka";
-      comment = "Kanban Project Management";
-      settings.StartupWMClass = "chrome-projects.${secrets.selfhosting.domain}__-Default";
-      exec = "chromium --ozone-platform-hint=auto --force-dark-mode --enable-features=WebUIDarkMode --app=\"https:projects.${secrets.selfhosting.domain}\" %U";
-      icon = "/home/${userOpts.username}/.local/share/icons/planka.png";
-      terminal = false;
-      categories = ["Application"];
-      type = "Application";
+      genericName = "Kanban Project Management";
+      icon = ./icons/planka.png;
+      categories = [
+        "Utility"
+      ];
     };
-
     ticktick = {
+      enable = true;
+      id = 5;
+      url = "https://ticktick.com";
       name = "TickTick";
-      comment = "To-Do List";
-      settings.StartupWMClass = "chrome-ticktick.com__-Default";
-      exec = "chromium --ozone-platform-hint=auto --force-dark-mode --enable-features=WebUIDarkMode --app=\"https:ticktick.com\" %U";
-      icon = "/home/${userOpts.username}/.local/share/icons/ticktick.png";
-      terminal = false;
-      categories = ["Application"];
-      type = "Application";
+      genericName = "Task Manager";
+      icon = ./icons/ticktick.png;
+      categories = [
+        "Utility"
+      ];
     };
-
-    # icon isn't working for duplicati - think the WMClass is wrong
-    duplicati = {
-      name = "Duplicati";
-      comment = "Backups";
-      settings.StartupWMClass = "chrome-localhost-8200__-Default";
-      exec = "chromium --ozone-platform-hint=auto --force-dark-mode --enable-features=WebUIDarkMode --app=\"http://localhost:8200\" %U";
-      icon = "/home/${userOpts.username}/.local/share/icons/duplicati.png";
-      terminal = false;
-      categories = ["Application"];
-      type = "Application";
-    };
-
     sketchup = {
+      enable = true;
+      id = 6;
+      url = "https:app.sketchup.com";
       name = "Sketchup";
-      comment = "Modeling";
-      settings.StartupWMClass = "chrome-app.sketchup.com__-Default";
-      exec = "chromium --ozone-platform-hint=auto --force-dark-mode --enable-features=WebUIDarkMode --app=\"https://app.sketchup.com\" %U";
-      icon = "/home/${userOpts.username}/.local/share/icons/sketchup.png";
-      terminal = false;
-      categories = ["Application"];
-      type = "Application";
+      genericName = "3D Design App";
+      icon = ./icons/sketchup.png;
+      categories = [
+        "Graphics"
+      ];
     };
-  };
-
-  # Icons
-  # check https://selfh.st/icons/
-
-  home.file."actual.png" = {
-    source = ./icons/actual.png;
-    target = ".local/share/icons/actual.png";
-  };
-
-  home.file."nextcloud.png" = {
-    source = ./icons/nextcloud.png;
-    target = ".local/share/icons/nextcloud.png";
-  };
-
-  home.file."silverbullet.png" = {
-    source = ./icons/silverbullet.png;
-    target = ".local/share/icons/silverbullet.png";
-  };
-
-  home.file."home-assistant.png" = {
-    source = ./icons/home-assistant.png;
-    target = ".local/share/icons/home-assistant.png";
-  };
-  home.file."planka.png" = {
-    source = ./icons/planka.png;
-    target = ".local/share/icons/planka.png";
-  };
-  home.file."ticktick.png" = {
-    source = ./icons/ticktick.png;
-    target = ".local/share/icons/ticktick.png";
-  };
-  home.file."duplicati.png" = {
-    source = ./icons/duplicati.png;
-    target = ".local/share/icons/duplicati.png";
-  };
-  home.file."sketchup.png" = {
-    source = ./icons/sketchup.png;
-    target = ".local/share/icons/sketchup.png";
   };
 }
