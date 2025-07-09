03f1dcd8fec8687078f2857440a8ba04127a6b88
ryan <105075689+rpdav@users.noreply.github.com>
2025-07-09 12:31:40 -0400

    added new proxy-conf module to service template

--- services/template/default.nix
+++ services/template/default.nix
@@ -2,27 +2,16 @@
   serviceOpts,
   config,
   ...
-}: let
-  proxy-conf = ''
-    # This is just a template based on common settings. If there's a template from swag available, replace this text with that.
-    server {
-        listen 443 ssl;
-        listen [::]:443 ssl;
-        server_name _SUBDOMAIN_.*;
-        include /config/nginx/ssl.conf;
-        client_max_body_size 0;
-        location / {
-            include /config/nginx/proxy.conf;
-            include /config/nginx/resolver.conf;
-            set $upstream_app _CONTAINER_;
-            set $upstream_port _PORT_;
-            set $upstream_proto http; #change to https if app requires
-            proxy_pass $upstream_proto://$upstream_app:$upstream_port;
-        }
-    }
-  '';
-in {
+}: {
   imports = [./docker-compose.nix];
+
+  # Create swag proxy config
+  virtualisation.oci-containers.proxy-conf."serviceName" = {
+    container = "containerName"; # defaults to serviceName if blank
+    subdomain = "www"; # defaults to serviceName if blank
+    port = 8080;
+    protocol = "http";
+  };
 
   # Create directories for appdata
   # d to create the directory, Z to recursively correct ownership (only needed when restoring from backup)
@@ -33,17 +22,4 @@ in {
 
   # pull secret env file
   sops.secrets."selfhosting/_CONTAINER_/env".owner = config.users.users.${serviceOpts.dockerUser}.name;
-
-  # Swag reverse proxy config
-  systemd.tmpfiles.settings."01-proxy-confs" = {
-    "${serviceOpts.dockerDir}/swag/proxy-confs/_CONTAINER_.subdomain.conf" = {
-      "f+" = {
-        group = "users";
-        user = "${serviceOpts.dockerUser}";
-        mode = "0700";
-        # Convert multiline string to single for tmpfiles
-        argument = proxy-conf;
-      };
-    };
-  };
 }
