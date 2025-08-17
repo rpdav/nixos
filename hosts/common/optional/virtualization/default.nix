8833e881954dc278a4d20183a91e64f4fde18574
ryan <105075689+rpdav@users.noreply.github.com>
2025-08-17 07:39:59 -0400

    virt tweaks to allow VM network access

--- hosts/common/optional/virtualization/default.nix
+++ hosts/common/optional/virtualization/default.nix
@@ -8,12 +8,16 @@
   environment.persistence."${config.systemOpts.persistVol}" = lib.mkIf config.systemOpts.impermanent {
     directories = [
       "/var/lib/libvirt"
+      "/etc/libvirt"
     ];
   };
   virtualisation.libvirtd = {
     enable = true;
     qemu.vhostUserPackages = with pkgs; [virtiofsd];
   };
+
+  networking.firewall.trustedInterfaces = ["virbr0"];
+
   programs.virt-manager.enable = true;
   users.users.ryan.extraGroups = ["libvirtd"];
   virtualisation.spiceUSBRedirection.enable = true;
