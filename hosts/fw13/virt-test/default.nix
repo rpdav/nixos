15b80d8b81c18d99d29088e15f380c13916c5b2e
ryan <105075689+rpdav@users.noreply.github.com>
2025-10-21 22:19:26 -0400

    moved network definition into nix config

--- hosts/fw13/virt-test/default.nix
+++ hosts/fw13/virt-test/default.nix
@@ -1,11 +1,18 @@
-{inputs, ...}: {
+{inputs, ...}: let
+  inherit (inputs) NixVirt;
+in {
   imports = [inputs.NixVirt.nixosModules.default];
   virtualisation.libvirt = {
     enable = true;
     connections."qemu:///system" = {
       networks = [
         {
-          definition = ./network.xml;
+          definition = NixVirt.lib.network.writeXML (NixVirt.lib.network.templates.bridge
+            {
+              name = "default";
+              uuid = "5046a610-afae-4b85-9f00-8beb9c101f95";
+              subnet_byte = 54;
+            });
           active = true;
         }
       ];
