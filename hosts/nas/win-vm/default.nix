772cee81a0f9bea713ea299f6088086422550407
ryan <105075689+rpdav@users.noreply.github.com>
2025-10-23 12:50:07 -0400

    added imperative and unraid win10 configs for nas

---
+++ hosts/nas/win-vm/default.nix
@@ -0,0 +1,27 @@
+{inputs, ...}: let
+  inherit (inputs) nixvirt;
+in {
+  imports = [inputs.nixvirt.nixosModules.default];
+  virtualisation.libvirt = {
+    enable = true;
+    connections."qemu:///system" = {
+      networks = [
+        {
+          definition = nixvirt.lib.network.writeXML (nixvirt.lib.network.templates.bridge
+            {
+              name = "default";
+              uuid = "5046a610-afae-4b85-9f00-8beb9c101f95";
+              subnet_byte = 54;
+            });
+          active = true;
+        }
+      ];
+      domains = [
+        {
+          definition = ./win10.xml;
+          active = true;
+        }
+      ];
+    };
+  };
+}
