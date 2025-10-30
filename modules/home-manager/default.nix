7b1dc3def69b33743dfb9e00950d1f965a6a452c
ryan <105075689+rpdav@users.noreply.github.com>
2025-10-29 21:47:39 -0400

    clean up docs

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,9 +1,4 @@
 {
-  #  imports = [
-  #    ./yubikey-touch-detector.nix
-  #    ./web-app.nix
-  #    ./monitors.nix
-  #  ];
   yubikey-touch-detector = import ./yubikey-touch-detector.nix;
   web-app = import ./web-app.nix;
   monitors = import ./monitors.nix;
--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,15 +1,4 @@
-# Add your reusable NixOS modules to this directory, on their own file (https://wiki.nixos.org/wiki/NixOS_modules).
-# These should be stuff you would like to share with others, not your personal configurations.
-# This is how EmergentMind set this up, but doesn't work for some reason:
-#{
-#  yubikey = import ./yubikey;
-#}
 {
-  #  imports = [
-  #    ./yubikey.nix
-  #    ./proxy-conf.nix
-  #    ./container-mount.nix
-  #  ];
   yubikey = import ./yubikey.nix;
   proxy-conf = import ./proxy-conf.nix;
   container-mount = import ./container-mount.nix;
