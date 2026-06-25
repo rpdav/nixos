72b03c8ecebfd5d9943640c3b9d6c12c23c459b8
ryan <105075689+rpdav@users.noreply.github.com>
2026-06-24 21:59:54 -0400

    starting to configure config.kdl

--- features/wm/niri/niri.nix
+++ features/wm/niri/niri.nix
@@ -1,8 +1,23 @@
 {...}: {
-  flake.nixosModules.niri = {lib, ...}: {
+  flake.nixosModules.niri = {
+    lib,
+    pkgs,
+    ...
+  }: {
     # Override hyprland auto-login while testing niri
     services.displayManager.autoLogin.enable = lib.mkForce false;
 
     programs.niri.enable = true;
+
+    # temporary simple packages while testing out imperative niri.kdl
+    environment.systemPackages = with pkgs; [
+      brightnessctl
+      playerctl
+      swayosd
+      networkmanagerapplet
+      swaylock
+      noctalia-shell
+      swayidle
+    ];
   };
 }
---
+++ niri.md
@@ -0,0 +1,3 @@
+## things to fix
+* how to get swayidle working as intended
+* make blueman-applet spawn floating and small
